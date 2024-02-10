051e9e64cdeda21b025c5cb94719b0802a350bbc
briskdust <briskdust@gmail.com>
2024-02-10 16:40:39 +0100

    feat: Replace TinyDB with MongoDB, and add docker compose

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,41 +1,33 @@
 import os
-import tempfile
-from functools import reduce
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-from tinydb import TinyDB, Query
-
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+mongo_uri = os.getenv('MONGO_URI')
+client = MongoClient(mongo_uri)
+db = client['students']
+collection = db['students']
 
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
-    if res:
+    if collection.find_one({'first_name': student.first_name, 'last_name': student.last_name}):
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
-    return student.student_id
+    student_id = collection.insert_one(student.to_dict()).inserted_id
+    return str(student_id)
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection.find_one({'_id': ObjectId(student_id)})
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
+    student['student_id'] = str(student['_id'])
+    del student['_id']  # Optional: remove the MongoDB specific _id if you want
     print(student)
     return student
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
+    result = collection.delete_one({'_id': ObjectId(student_id)})
+    if result.deleted_count == 0:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
     return student_id
